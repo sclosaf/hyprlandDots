ce53d0a4a8404ffff0586c97ca5f5ea6ea15b9df
sclosaf <sclosaf@gmail.com>
2025-09-10 06:59:30 +0000

    update

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,4 @@
-e!/bin/bash
+#!/bin/bash
 
 source ./source/mapping.sh
 
@@ -7,21 +7,15 @@ main()
     echo "Copying configuration file ..."
 
     for src in "${!mapping[@]}"; do
+
         local trg="${mapping[$src]}"
-
-        if [[ -e "$trg" ]]; then
-            echo "Removing existing $trg ..."
-            rm -rf "$trg"
-        fi
-
         mkdir -p "$(dirname "$trg")" || {
             echo "Failed to create directory for $trg"
             continue
         }
 
         if [[ -e "../$src" ]]; then
-            echo "Copying $(basename "$src") to $trg ..."
-
+            echo "Copying $(basename "$src") to $trg..."
             if [[ -d "../$src" ]]; then
                 cp -r "../$src" "$(dirname "$trg")" || {
                     echo "Error copying directory $src"
@@ -32,7 +26,7 @@ main()
                 }
             fi
         else
-            echo -e "Warning: ../$src not found, skipping ..."
+            echo  "Warning: $src not found, skipping...${NC}"
         fi
     done
 
