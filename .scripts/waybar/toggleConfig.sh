6221e79c5885de101c0dbc27980cab80ae16bbd9
sclosaf <sclosaf@gmail.com>
2025-09-17 11:40:21 +0200

    update

---
+++ .scripts/waybar/toggleConfig.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+WAYBAR_DIR="$HOME/.config/waybar"
+TOPBAR_CONFIG="$WAYBAR_DIR/topbar/config.jsonc"
+TOPBAR_STYLE="$WAYBAR_DIR/topbar/style.css"
+SIDEBAR_CONFIG="$WAYBAR_DIR/sidebar/config.jsonc"
+SIDEBAR_STYLE="$WAYBAR_DIR/sidebar/style.css"
+
+getCurrentConfig()
+{
+    pgrep -fa "waybar.*-c.*sidebar" > /dev/null && echo "sidebar" || echo "topbar"
+}
+
+switchConfig()
+{
+    local currentConfig=$(getCurrentConfig)
+    
+    pkill -x waybar
+    
+    # Start waybar with the opposite config
+    if [[ "$currentConfig" == "topbar" ]]; then
+        waybar -c "$SIDEBAR_CONFIG" -s "$SIDEBAR_STYLE" &
+    else
+        waybar -c "$TOPBAR_CONFIG" -s "$TOPBARSTYLE" &
+    fi
+}
+
+switchConfig
