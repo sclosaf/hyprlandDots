38f070e460a877f864050531542a8cb539e6abbf
sclosaf <sclosaf@gmail.com>
2025-09-27 00:19:00 +0200

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -104,3 +104,34 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.opt.errorbells = false
 vim.opt.visualbell = true
 vim.opt.belloff = "all"
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "gbprod/nord.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("nord").setup({})
+      vim.cmd.colorscheme("nord")
+    end,
+  },
+}, {
+  install = {
+    colorscheme = { "nord" },
+  }
+})
+
+vim.cmd.colorscheme("nord")
