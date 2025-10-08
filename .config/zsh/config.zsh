be7bd3e44f08c0cd0919fb23ac0a530ade59f8bc
sclosaf <sclosaf@gmail.com>
2025-10-08 20:59:02 +0200

    update

--- .config/zsh/config.zsh
+++ .config/zsh/config.zsh
@@ -1,5 +1,1 @@
 setopt appendhistory sharehistory hist_ignore_space hist_ignore_all_dups hist_save_no_dups hist_ignore_dups hist_find_no_dups
-
-autoload -U edit-command-line
-zle -N edit-command-line
-bindkey '^e' edit-command-line
