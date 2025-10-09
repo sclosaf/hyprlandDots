f84a96a4ba6c90004297c326d9cff85e8a129b50
sclosaf <sclosaf@gmail.com>
2025-10-09 22:16:57 +0200

    update

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,6 +1,5 @@
 zsh_add_plugin "zsh-users/zsh-completions"
 zsh_add_plugin "zsh-users/zsh-autosuggestions"
 zsh_add_plugin "Aloxaf/fzf-tab"
-zsh_add_plugin "zap-zsh/vim"
 
 zstyle ':fzf-tab:*' use-fzf-default-opts yes
