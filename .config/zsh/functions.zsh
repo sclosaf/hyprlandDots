656ffb77e5e63ba10b9dc89f44f4bebd7600bd7c
sclosaf <sclosaf@gmail.com>
2025-10-14 17:13:21 +0200

    update

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -21,56 +21,25 @@ function git_formatter()
 
     if [[ -n "$vcs_info_msg_0_" ]]; then
         local git_info="$vcs_info_msg_0_"
-        local additional_info=""
+        local symbols=""
 
-        local staged_count=$(git diff --staged --name-only 2>/dev/null | wc -l | tr -d ' ')
-        if [[ $staged_count -gt 0 ]]; then
-            additional_info+="${VCS_MODIFIED_COLOR}+${staged_count}%f "
+        if [[ -n "$(git status --porcelain 2>/dev/null)" ]]; then
+            symbols+="${VCS_MODIFIED_COLOR}!%f"
         fi
 
-        local unstaged_count=$(git diff --name-only 2>/dev/null | wc -l | tr -d ' ')
-        if [[ $unstaged_count -gt 0 ]]; then
-            additional_info+="${VCS_MODIFIED_COLOR}!${unstaged_count}%f "
-        fi
-
-        local ahead_behind=$(git rev-list --left-right --count HEAD...@'{u}' 2>/dev/null)
-        if [[ -n "$ahead_behind" ]]; then
-            local ahead=$(echo $ahead_behind | cut -f1)
-            local behind=$(echo $ahead_behind | cut -f2)
-
-            if [[ $behind -gt 0 ]]; then
-                additional_info+="${VCS_CLEAN_COLOR}<${behind}%f "
-            fi
-
+        local upstream=$(git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null)
+        if [[ -n "$upstream" ]]; then
+            local ahead=$(git rev-list --count HEAD.."$upstream" 2>/dev/null)
             if [[ $ahead -gt 0 ]]; then
-                if [[ $behind -gt 0 ]]; then
-                    additional_info+="${VCS_CLEAN_COLOR}>${ahead}%f "
-                else
-                    additional_info+="${VCS_CLEAN_COLOR} >${ahead}%f "
-                fi
+                symbols+="${VCS_CLEAN_COLOR}?%f"
             fi
         fi
 
-        local stash_count=$(git stash list 2>/dev/null | wc -l | tr -d ' ')
-        if [[ $stash_count -gt 0 ]]; then
-            additional_info+="${VCS_CLEAN_COLOR}*${stash_count}%f "
+        if [[ -z "$symbols" ]]; then
+            symbols+="${VCS_CLEAN_COLOR}>%f"
         fi
 
-        local untracked_count=$(git status --porcelain 2>/dev/null | grep '^??' | wc -l | tr -d ' ')
-        if [[ $untracked_count -gt 0 ]]; then
-            additional_info+="${VCS_UNTRACKED_COLOR}?${untracked_count}%f "
-        fi
-
-        local conflict_count=$(git diff --name-only --diff-filter=U 2>/dev/null | wc -l | tr -d ' ')
-        if [[ $conflict_count -gt 0 ]]; then
-            additional_info+="${VCS_CONFLICTED_COLOR}~${conflict_count}%f "
-        fi
-
-        if [[ -n "$additional_info" ]]; then
-            echo "${git_info} ${additional_info}"
-        else
-            echo "$git_info"
-        fi
+        echo "${git_info} ${symbols}"
     fi
 }
 
