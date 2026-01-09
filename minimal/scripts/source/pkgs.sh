e88f7d169f283dd6e087ed2b37f698ac31803290
sclosaf <sclosaf@gmail.com>
2026-01-09 21:57:01 +0100

    update

--- minimal/scripts/source/pkgs.sh
+++ minimal/scripts/source/pkgs.sh
@@ -38,4 +38,14 @@ export packages=(
     "upower"
     "powertop"
     "luarocks"
+
+    "nvidia-open"
+    "nvidia-utils"
+    "nvidia-settings"
+    "nvidia-prime"
+    "mesa"
+    "vulkan-intel"
+    "intel-media-driver"
+    "vulkan-icd-loader"
+    "linux-headers"
 )
